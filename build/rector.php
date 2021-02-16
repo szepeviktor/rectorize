4a0632d6260cc3647a5678d33ca9e67b1677f189
Viktor Szépe <viktor@szepe.net>
2021-02-16 10:48:01 +0100

    Create rector.php

---
+++ build/rector.php
@@ -0,0 +1,12 @@
+<?php
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\DowngradeSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SETS, [
+        DowngradeSetList::PHP_80,
+    ]);
+};
