10dde8d502af525753d2dfd16e9ffa6f462a76b0
Viktor Szépe <viktor@szepe.net>
2021-02-17 06:32:43 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -8,5 +8,25 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::SETS, [
         DowngradeSetList::PHP_80,
+
+        //PHP7.3 DowngradeSetList::PHP_74,
+
+        //PHP7.2 DowngradeSetList::PHP_74,
+        //PHP7.2 DowngradeSetList::PHP_73,
+
+        //PHP7.1 DowngradeSetList::PHP_74,
+        //PHP7.1 DowngradeSetList::PHP_73,
+        //PHP7.1 DowngradeSetList::PHP_72,
+
+        //PHP7.0 DowngradeSetList::PHP_74,
+        //PHP7.0 DowngradeSetList::PHP_73,
+        //PHP7.0 DowngradeSetList::PHP_72,
+        //PHP7.0 DowngradeSetList::PHP_71,
+
+        //PHP5.6 DowngradeSetList::PHP_74,
+        //PHP5.6 DowngradeSetList::PHP_73,
+        //PHP5.6 DowngradeSetList::PHP_72,
+        //PHP5.6 DowngradeSetList::PHP_71,
+        //PHP5.6 DowngradeSetList::PHP_70,
     ]);
 };
